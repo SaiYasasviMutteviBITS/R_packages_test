3659d84d9f0310a3fb01bbb299b3007493d007c4
SaiYasasviMutteviBITS <113597600+SaiYasasviMutteviBITS@users.noreply.github.com>
2022-11-25 21:54:23 +0530

    Add files via upload

---
+++ R/SumOfSquaresOfDeviation.R
@@ -0,0 +1,23 @@
+#' Provides function to find sum of squares of deviation from mean
+#' @param vector1 A distribution vector.
+#' @return z A sum of squares of deviation - scalar.
+#' @examples
+#'
+#' SSD(c(1,2,3,4,5))
+#'
+#'
+#Function to find sum of squares of deviation from mean
+
+SSD <- function(vector1){
+  su=sum(vector1**2)
+  me=mean(vector1)
+  n=length(vector1)
+  z= su-(n*(me^2))
+  return(z)
+}
+
+#Example
+#SSD(c(1,2,3,4,5))
+#Answer -  10
+SSD(c(1,2,3,4))
+
